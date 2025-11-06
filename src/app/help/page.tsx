6c6df4982b2656c069134ccf709bf811faa99db9
Yehor <yehor.v@aquinas.digital>
2025-11-06 11:16:09 +0200

    test 13

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis earum iste, quam quas est atque. Architecto, incidunt dignissimos ullam omnis rem natus nulla eaque modi ab sequi accusamus. Voluptatibus, quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quam ab veritatis aspernatur possimus nisi totam, voluptate, laboriosam expedita deleniti rem! Maxime enim cumque nulla tempore et at, repellat iste?Lorem ipsum dolor sit amet consectetur adipisicing elit. Error inventore harum sapiente architecto dolor quibusdam. Laboriosam pariatur, quia numquam incidunt molestiae explicabo velit? Alias voluptatem rem sed perspiciatis iusto officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ratione accusantium quo eligendi delectus, illum qui cumque animi eaque rem provident corrupti optio aperiam facere, quidem consectetur, nostrum numquam suscipit?
         1111111111111111222222233333
 
-    <img src="/test.png" alt="test" className="w-84 h-40"/>
+    <img src="/test2.png" alt="test" className="w-84 h-40"/>
 
       <Link target="_blank"
       rel="noopener noreferrer" href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent("https://test-meta-liart.vercel.app/")}`} >
